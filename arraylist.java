c696635243314a21177999fe1008bf8ed6fe5a30
Gopinathpitchai <pgopinathpitchai@gmail.com>
2026-07-29 14:42:26 +0530

    29/07/2026

---
+++ arraylist.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class arraylist{
+    public static void main(String[]args){
+        ArrayList<String>names =new ArrayList<>();
+
+        names.add("Gopi");
+        names.add("Goku");
+        names.add("Karthik");
+        names.add("Saravananun");
+
+        System.out.println(names);
+
+        
+    }
+}
